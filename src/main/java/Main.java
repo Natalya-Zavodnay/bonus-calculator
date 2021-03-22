89eafac7d24ba3f9898251bd84d9108a7d07cc68
Natalya-Zavodnay <nz41330@gmail.com>
2021-03-22 11:04:48 +0300

    edited commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,12 +4,9 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
-    long expected = 30;
 
-
-    long actual = service.calculate(amount, registered);
-    boolean passed = expected == actual;
-    System.out.println(passed);
+    long total = service.calculate (amount,registered);
+    System.out.println(total);
 
   }
 }
